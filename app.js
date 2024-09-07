a14ddb6762fe69d966b43ac688600b16d2845931
Pedro Marques <pjjfmarques@gmail.com>
2024-09-06 22:58:00 -0300

    Aula 05

--- app.js
+++ app.js
@@ -9,23 +9,45 @@
 function pesquisar() {
     // Seleciona a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
-  
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+    //console.log(campoPesquisa);
+    
+    if (!campoPesquisa) {
+      section.innerHTML = "<p>Procura sem dados</p>"
+      return;
+    }
+
     // Inicializa uma string para armazenar os resultados em HTML
     let resultados = "";
-  
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
     // Itera sobre cada dado e cria o HTML para o resultado
     for (let dado of dados) {
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href="${dado.link}" target="_blank">Para mais informações</a>
-        </div>
-      `;
+      titulo=dado.titulo.toLowerCase();
+      descricao=dado.descricao.toLocaleLowerCase();
+      tags=dado.tags.toLocaleLowerCase();
+
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        //console.log("Encontrado")
+        resultados += `
+          <div class="item-resultado">
+            <h2>
+              <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Para mais informações</a>
+          </div>
+        `;
+      }
     }
-  
+
+    if (!resultados) {
+      resultados = "<p>Não encontrado</p>"
+    }
+    
     // Atualiza o conteúdo da seção com os resultados gerados
     section.innerHTML = resultados;
   }
